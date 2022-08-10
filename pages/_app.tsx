1b1600ba8312bd50dc54cf529fd4baf424474741
Rifqi Pratama <rifqipratama9@gmail.com>
2022-08-10 23:34:41 +0700

    refactor login navbar component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import '../styles/overview.css';
 import '../styles/transactions.css';
 import '../styles/transactions-detail.css';
 import '../styles/edit-profile.css';
+import '../styles/navbar-log-in.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
@@ -29,10 +30,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         {/* AOS */}
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-
-        {/* Bootstrap Js */}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous" />
+    
       </Head>
+
+      {/* Bootstrap Js */}
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous" />
+      
       <Component {...pageProps} />
     </>
   )
