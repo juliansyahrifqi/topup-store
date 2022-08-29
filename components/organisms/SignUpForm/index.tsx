ed8ee96d82738fbda866d021818443842be0f545
Rifqi Pratama <rifqipratama9@gmail.com>
2022-08-29 23:48:14 +0700

    refactor sign up page

--- components/organisms/SignUpForm/index.tsx
+++ components/organisms/SignUpForm/index.tsx
@@ -1,18 +1,26 @@
 import Link from 'next/link';
 
-export default function SignInForm() {
+export default function SignUpForm() {
   return (
     <>
-      <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign In</h2>
-
-      <p className="text-lg color-palette-1 m-0">Masuk untuk melakukan proses top up</p>
-
+      <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign Up</h2>
+      <p className="text-lg color-palette-1 m-0">Daftar dan bergabung dengan kami</p>
       <div className="pt-50">
+        <label htmlFor="name" className="form-label text-lg fw-medium color-palette-1 mb-10">Full Name</label>
+        <input
+          type="text"
+          className="form-control rounded-pill text-lg"
+          id="name"
+          name="name"
+          aria-describedby="name"
+          placeholder="Enter your name"
+        />
+      </div>
+      <div className="pt-30">
         <label htmlFor="email" className="form-label text-lg fw-medium color-palette-1 mb-10">
           Email
           Address
         </label>
-
         <input
           type="email"
           className="form-control rounded-pill text-lg"
@@ -22,15 +30,8 @@ export default function SignInForm() {
           placeholder="Enter your email address"
         />
       </div>
-
       <div className="pt-30">
-        <label
-          htmlFor="password"
-          className="form-label text-lg fw-medium color-palette-1 mb-10"
-        >
-          Password
-        </label>
-
+        <label htmlFor="password" className="form-label text-lg fw-medium color-palette-1 mb-10">Password</label>
         <input
           type="password"
           className="form-control rounded-pill text-lg"
@@ -40,24 +41,23 @@ export default function SignInForm() {
           placeholder="Your password"
         />
       </div>
-
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <Link href="/">
+        <Link href="sign-up-photo">
           <a
-            className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
+            className="btn btn-sign-up fw-medium text-lg text-white rounded-pill mb-16"
             role="button"
           >
-            Continue to Sign In
+            Continue
           </a>
         </Link>
 
-        <Link href="/sign-up">
+        <Link href="sign-in">
           <a
-            className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
+            className="btn btn-sign-in fw-medium text-lg color-palette-1 rounded-pill"
             role="button"
           >
             Sign
-            Up
+            In
           </a>
         </Link>
       </div>
