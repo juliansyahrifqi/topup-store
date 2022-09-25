930b8ffc33e886ac730e843e4783796e8db2d0fc
Rifqi Pratama <rifqipratama9@gmail.com>
2022-09-25 22:48:16 +0700

    add data type for api game item

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import GameItem from '../../molecules/GameItem';
 import { getFeaturedGame } from '../../../services/player';
+import { GameItemTypes } from '../../../services/data-types';
 
 export default function FeaturedGame() {
   const [gameList, setGameList] = useState([]);
@@ -15,6 +16,9 @@ export default function FeaturedGame() {
     getFeaturedGameList();
   }, []);
 
+  // URL image from API
+  const API_IMAGE = process.env.NEXT_PUBLIC_IMG;
+
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -28,8 +32,13 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          {gameList.map((item) => (
-            <GameItem key={item._id} thumbnail={`https://topupstore.herokuapp.com/uploads/${item.thumbnail}`} title={item.name} category={item.category.name} />
+          {gameList.map((item: GameItemTypes) => (
+            <GameItem
+              key={item._id}
+              thumbnail={`${API_IMAGE}/${item.thumbnail}`}
+              title={item.name}
+              category={item.category.name}
+            />
           ))}
         </div>
       </div>
