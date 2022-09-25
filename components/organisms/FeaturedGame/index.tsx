7b2374c3d7da7b14edf5e0ae8d1a93eb986eb472
Rifqi Pratama <rifqipratama9@gmail.com>
2022-09-25 22:23:57 +0700

    test sample api

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import GameItem from '../../molecules/GameItem';
 
 export default function FeaturedGame() {
+  const [gameList, setGameList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('https://topupstore.herokuapp.com/api/v1/players/landingpage');
+
+      setGameList(response.data.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -14,11 +28,15 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          <GameItem thumbnail="/img/Thumbnail-1.png" title="Super Mechs" category="Mobile" />
+          {gameList.map((item) => (
+            <GameItem key={item._id} thumbnail={`https://topupstore.herokuapp.com/uploads/${item.thumbnail}`} title={item.name} category={item.category.name} />
+          ))}
+
+          {/* <GameItem thumbnail="/img/Thumbnail-1.png" title="Super Mechs" category="Mobile" />
           <GameItem thumbnail="/img/Thumbnail-2.png" title="Call of Duty: Modern" category="Mobile" />
           <GameItem thumbnail="/img/Thumbnail-3.png" title="Mobile Legends" category="Mobile" />
           <GameItem thumbnail="/img/Thumbnail-4.png" title="Clash of Clans" category="Mobile" />
-          <GameItem thumbnail="/img/Thumbnail-5.png" title="Valorant" category="Desktop" />
+          <GameItem thumbnail="/img/Thumbnail-5.png" title="Valorant" category="Desktop" /> */}
 
         </div>
       </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+/* eslint linebreak-style: ["error", "unix"] */
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['https://topupstore.herokuapp.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
