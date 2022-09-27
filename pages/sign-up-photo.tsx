2327399d42bcef6302594d1240649805f1a17402
Rifqi Pratama <rifqipratama9@gmail.com>
2022-09-27 17:47:48 +0700

    add sign up process

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -2,15 +2,18 @@
 
 import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
+import { setSignUp } from '../services/auth';
 import { getGameCategory } from '../services/player';
 
 export default function SignUpPhoto() {
   const [categories, setCategories] = useState([]);
   const [favorite, setFavorite] = useState('');
-
-  const onSubmit = () => {
-    console.log('Favorite', favorite);
-  };
+  const [image, setImage] = useState('');
+  const [imagePreview, setImagePreview] = useState('/icon/upload.svg');
+  const [localForm, setLocalForm] = useState({
+    name: '',
+    email: '',
+  });
 
   const getGameCategoryAPI = useCallback(async () => {
     const data = await getGameCategory();
@@ -21,7 +24,30 @@ export default function SignUpPhoto() {
 
   useEffect(() => {
     getGameCategoryAPI();
+
+    const getLocalForm = localStorage.getItem('sign-up-form');
+    setLocalForm(JSON.parse(getLocalForm));
   }, []);
+
+  const onSubmit = async () => {
+    const localStorageItem = await localStorage.getItem('sign-up-form');
+    const form = JSON.parse(localStorageItem);
+
+    const data = new FormData();
+
+    data.append('image', image);
+    data.append('email', form.email);
+    data.append('password', form.password);
+    data.append('phoneNumber', '08123456789');
+    data.append('username', form.name);
+    data.append('name', form.name);
+    data.append('role', 'user');
+    data.append('status', 'Y');
+    data.append('favorite', favorite);
+
+    const result = await setSignUp(data);
+    console.log(result);
+  };
 
   return (
     <section className="sign-up-photo mx-auto pt-lg-227 pb-lg-227 pt-130 pb-50">
@@ -32,13 +58,22 @@ export default function SignUpPhoto() {
               <div className="mb-20">
                 <div className="image-upload text-center">
                   <label htmlFor="avatar">
-                    <Image src="/icon/upload.svg" width={120} height={120} alt="upload-icon" />
+                    <Image src={imagePreview} width={120} height={120} className="img-upload" alt="upload-icon" />
                   </label>
-                  <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                  <input
+                    id="avatar"
+                    type="file"
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={(event) => {
+                      setImagePreview(URL.createObjectURL(event.target.files[0]));
+                      setImage(event.target.files[0]);
+                    }}
+                  />
                 </div>
               </div>
-              <h2 className="fw-bold text-xl text-center color-palette-1 m-0">Shayna Anne</h2>
-              <p className="text-lg text-center color-palette-1 m-0">shayna@anne.com</p>
+              <h2 className="fw-bold text-xl text-center color-palette-1 m-0">{localForm.name}</h2>
+              <p className="text-lg text-center color-palette-1 m-0">{localForm.email}</p>
               <div className="pt-50 pb-50">
                 <label htmlFor="category" className="form-label text-lg fw-medium color-palette-1 mb-10">
                   Favorite
@@ -54,6 +89,7 @@ export default function SignUpPhoto() {
                 >
                   {categories.map((category) => (
                     <option
+                      key={category._id}
                       value={category._id}
                     >
                       {category.name}
---
+++ services/auth.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const ROOT_API = process.env.NEXT_PUBLIC_API;
+const API_VERSION = 'api/v1';
+
+export const setSignUp = async (data) => {
+  const URL = 'auth/signup';
+
+  const response = await axios.post(`${ROOT_API}/${API_VERSION}/${URL}`, data);
+  const axiosResponse = response.data;
+
+  return axiosResponse.data;
+};
+
+export const setLogin = async () => null;
