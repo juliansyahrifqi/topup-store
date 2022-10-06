e351117e1d5329f30952643f2d257da317305ff2
Rifqi Pratama <rifqipratama9@gmail.com>
2022-10-06 18:34:27 +0700

    update profile (error)

--- pages/member/edit-profile.tsx
+++ pages/member/edit-profile.tsx
@@ -1,9 +1,56 @@
 /* eslint linebreak-style: ["error", "unix"] */
 
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import Input from '../../components/atoms/Input';
 import Sidebar from '../../components/organisms/Sidebar';
+import { JWTPayloadTypes, UserTypes } from '../../services/data-types';
+import { updateProfile } from '../../services/member';
 
 export default function EditProfile() {
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    avatar: '',
+  });
+  const [imagePreview, setImagePreview] = useState(null);
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userFromPayload: UserTypes = payload.player;
+
+      setUser(userFromPayload);
+    }
+  }, []);
+
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    const data = new FormData();
+
+    data.append('image', user.avatar);
+    data.append('name', user.name);
+
+    // console.log(user.avatar);
+    const response = await updateProfile(data, user.id);
+
+    // console.log(response);
+    if (response.error) {
+      toast.error(response.message);
+    } else {
+      console.log(response);
+    }
+  };
+
   return (
     <section className="edit-profile overflow-auto">
       <Sidebar activeMenu="settings" />
@@ -15,40 +62,45 @@ export default function EditProfile() {
           <div className="bg-card pt-30 ps-30 pe-30 pb-30">
             <form action="">
               <div className="photo d-flex">
-                <div className="position-relative me-20">
-                  <img src="/img/avatar-1.png" width="90" height="90" className="avatar img-fluid" alt="avatar" />
-                  <div
-                    className="avatar-overlay position-absolute top-0 d-flex justify-content-center align-items-center"
-                  >
-                    <img src="/icon/upload.svg" alt="icon-upload" width={90} height={90} />
-                  </div>
-                </div>
-
                 <div className="image-upload">
                   <label htmlFor="avatar">
-                    <img src="/icon/upload.svg" alt="icon-upload" width={90} height={90} />
+                    {imagePreview ? (
+                      <img src={imagePreview} alt="icon-upload" width={90} height={90} style={{ borderRadius: '100%' }} />
+                    ) : (
+                      <img src={`${IMG}/${user.avatar}`} alt="icon-upload" width={90} height={90} style={{ borderRadius: '100%' }} />
+                    )}
                   </label>
-                  <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                  <input
+                    id="avatar"
+                    type="file"
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={(event) => {
+                      setImagePreview(URL.createObjectURL(event.target.files[0]));
+                      setUser({ ...user, avatar: event.target.files[0] });
+                    }}
+                  />
                 </div>
 
               </div>
 
               <div className="pt-30">
-                <Input label="Full Name" placeholder="Enter your full name" />
+                <Input label="Full Name" placeholder="Enter your full name" value={user.name} onChange={(event) => setUser({ ...user, name: event.target.value })} />
               </div>
 
               <div className="pt-30">
-                <Input label="Email Address" placeholder="Enter your email address" />
+                <Input label="Email Address" placeholder="Enter your email address" disabled value={user.email} />
               </div>
 
-              <div className="pt-30">
+              {/* <div className="pt-30">
                 <Input label="Phone" placeholder="Enter your phone number" />
-              </div>
+              </div> */}
 
               <div className="button-group d-flex flex-column pt-50">
                 <button
-                  type="submit"
+                  type="button"
                   className="btn btn-save fw-medium text-lg text-white rounded-pill"
+                  onClick={onSubmit}
                 >
                   Save My Profile
                 </button>
--- services/member.ts
+++ services/member.ts
@@ -40,3 +40,14 @@ export async function getTransactionDetail(id: string, token: string) {
     serverToken: token,
   });
 }
+
+export async function updateProfile(data: FormData, id: string) {
+  const url = `${ROOT_API}/${API_VERSION}/players/profile/${id}`;
+
+  return callAPI({
+    url,
+    method: 'PUT',
+    data,
+    token: true,
+  });
+}
